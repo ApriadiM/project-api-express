7ac14c0cd9fdb86ae3a3f076fbe31e65bf20db70
Apriadi <apriadim>
2019-12-04 15:45:47 +0700

    add enkripsi password

--- routes/users/controller.js
+++ routes/users/controller.js
@@ -1,6 +1,7 @@
 const { user: users } = require("../../models");
 const { get } = require("../../config");
 const objectId = require("mongodb").ObjectId;
+const { hashPassword, comparedPassword } = require("../../helpers");
 
 module.exports = {
     getAll: (req, res) => {
@@ -47,10 +48,14 @@ module.exports = {
                 console.log(error);
             });
     },
-    addOne: (req, res) => {
+    addOne: async (req, res) => {
+        const hash = await hashPassword(req.body.password);
+        console.log(hash);
+        
+
         get()
             .collection("users")
-            .insertOne(req.body)
+            .insertOne({...req.body, password: hash})
             .then(result => {
                 res.status(201).json({
                     message: "Data successfully added",
@@ -76,18 +81,25 @@ module.exports = {
                 console.log(error);
             });
     },
-    login: (req, res) => {
+    login: async (req, res) => {
         const { body } = req;
 
         get()
             .collection("users")
-            .findOne({ email: body.email, password: body.password })
-            .then(response => {
-                const { email, firstName } = response;
-                res.status(200).json({
-                    message: "Login successfull",
-                    data: { email, firstName }
-                });
+            .findOne({ email: body.email })
+            .then(async response => {
+                const compared = await comparedPassword(
+                    req.body.password,
+                    response.password
+                );
+
+                if (compared === true) {
+                    const { email, firstName } = response;
+                    res.status(200).json({
+                        message: "Login successfull",
+                        data: { email, firstName }
+                    });
+                }
             });
     }
 };
